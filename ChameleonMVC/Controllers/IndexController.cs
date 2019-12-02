ebe2174c10f6e19e8ea4367e34fd682133e1cc17
Dani Pernando <DANI.PERNANDO@BINTANG7.COM>
2019-12-02 15:57:35 +0700

    first commit

---
+++ ChameleonMVC/Controllers/IndexController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChameleonMVC.Controllers
+{
+    public class IndexController : Controller
+    {
+        //
+        // GET: /Index/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult HomePage()
+        {
+            return View();
+        }
+
+        public ActionResult Catalog()
+        {
+            return View();
+        }
+
+        public ActionResult CreateDisposal()
+        {
+            return View();
+        }
+
+        public ActionResult DisposalList()
+        {
+            return View();
+        }
+
+        public ActionResult DisposalDetail()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Home()
+        {
+            return View();
+        }
+
+        public ActionResult DisposalApprovalList()
+        {
+            return View();
+        }
+
+        public ActionResult DisposalApprovalForm()
+        {
+            return View();
+        }
+
+        public ActionResult BidHistory()
+        {
+            return View();
+        }
+    }
+}
---
+++ ChameleonMVC/obj/Release/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ChameleonMVC.MvcApplication" Language="C#" %>
