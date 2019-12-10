c2006f5d78612c4773cfab858f1a055fa6aad6ff
Dani Pernando <cstrife702@gmail.com>
2019-12-10 15:49:07 +0700

    Generate Disposal Number

--- ChameleonMVC/Controllers/IndexController.cs
+++ ChameleonMVC/Controllers/IndexController.cs
@@ -1,5 +1,9 @@
-﻿using System;
+﻿using ChameleonMVC.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -63,6 +67,80 @@ namespace ChameleonMVC.Controllers
         public ActionResult BidHistory()
         {
             return View();
+        }
+
+        public ActionResult GetLOBMasterData()
+        {
+            List<string> ModelData = new List<string>();
+
+            DataTable dt = new DataTable();
+            ConnectionStringSettings mySetting = ConfigurationManager.ConnectionStrings["LelangGA"];
+            string conString = mySetting.ConnectionString;
+            string query = "exec [dbo].[STP_DDLMaster]";
+
+            SqlConnection conn = new SqlConnection(conString);
+            SqlCommand cmd = new SqlCommand(query, conn);
+            conn.Open();
+            using (SqlCommand command = new SqlCommand("[dbo].[STP_DDLMaster]", conn))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add("@Option", System.Data.SqlDbType.Int);
+                command.Parameters["@Option"].Value = 1;
+
+                SqlDataAdapter dataAdapt = new SqlDataAdapter();
+                dataAdapt.SelectCommand = command;
+
+                dataAdapt.Fill(dt);
+            }
+            conn.Close();
+
+            List<DataRow> DataRow = new List<DataRow>();
+            int n = 0;
+            foreach (DataRow dr in dt.Rows)
+            {
+                ModelData.Add(dr[0].ToString());
+            }
+
+            return Json(ModelData);
+        }
+
+        public ActionResult GenerateDisposalNumber(GenerateDisposal Model)
+        {
+            List<string> ModelData = new List<string>();
+            ConnectionStringSettings mySetting = ConfigurationManager.ConnectionStrings["LelangGA"];
+            string conString = mySetting.ConnectionString;
+            string query = "exec [dbo].[STP_CreateDisposal_Transc]";
+            string Result;
+
+            SqlConnection conn = new SqlConnection(conString);
+            SqlCommand cmd = new SqlCommand(query, conn);
+            conn.Open();
+            using (SqlCommand command = new SqlCommand("[dbo].[STP_CreateDisposal_Transc]", conn))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add("@Option", System.Data.SqlDbType.Int);
+                command.Parameters["@Option"].Value = 1;
+
+                command.Parameters.Add("@LOBGET", System.Data.SqlDbType.NVarChar);
+                command.Parameters["@LOBGET"].Value = Model.CostCenter;
+
+                command.Parameters.Add("@PICGet", System.Data.SqlDbType.NVarChar);
+                command.Parameters["@PICGet"].Value = Model.PIC;
+
+                command.Parameters.Add("@LokasiGet", System.Data.SqlDbType.NVarChar);
+                command.Parameters["@LokasiGet"].Value = Model.Lokasi;
+
+                command.Parameters.Add("@AlasanLelangGet", System.Data.SqlDbType.NVarChar);
+                command.Parameters["@AlasanLelangGet"].Value = Model.Alasan;
+
+                command.Parameters.Add("@UsernameGet", System.Data.SqlDbType.NVarChar);
+                command.Parameters["@UsernameGet"].Value = "Admin";
+
+                Result = (string)command.ExecuteScalar();
+
+            }
+            conn.Close();
+            return Json(ModelData);
         }
     }
 }
---
+++ ChameleonMVC/Models/DisposalCreatorModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChameleonMVC.Models
+{
+    public class DisposalCreatorModel
+    {
+    }
+    public class GenerateDisposal
+    {
+        public string CostCenter { get; set; }
+        public string Lokasi { get; set; }
+        public string BagianTeknik { get; set; }
+        public string Alasan { get; set; }
+        public string PIC { get; set; }
+    }
+}
